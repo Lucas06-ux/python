2fd727eba48191bebf92892b5348ff8e40147f3b
Lucas06-ux <lubgian@gmail.com>
2025-03-21 19:59:32 -0300

    Adicionei um exemplo de código para calcular a média de dois CPs

---
+++ CalculoCP.py
@@ -0,0 +1,20 @@
+#Algoritmo para calcular a média de 2 CPs
+ 
+'''Rotina calcula a média de dois CPs no Semestre'''
+
+CP1=0 
+CP2 = 0
+Soma = 0
+Media = 0
+
+print("Programa para calcular a média de duas provas")
+
+CP1= float(input("Informe a nota do CP1: \n"))
+
+CP2 = float(input("Informe a nota do CP2 : \n"))
+
+Soma = CP1 + CP2
+Media = Soma/2
+print("Sua média é: ")
+print(Media)
+print("Bons estudos")
