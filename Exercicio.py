9d0d3ee0e2b42195881741b53c7f154c7dbe3363
Lucas06-ux <lubgian@gmail.com>
2025-03-28 23:17:03 -0300

    Exercícios resolvidos na aula

--- Exercicio.py
+++ Exercicio.py
@@ -1,3 +1,80 @@
+print('A. Considere as variáveis A = 11 e B = 3 e calcule as seguintes expressões')
 A = 11
 B = 3
-print( A + B * A)
+print(f'O resultado de A + b * A é: {A + B * A}')
+
+print(f'O resultado de B * A + B é: {B * A + B}')
+
+print(f'O resultado de A // B é: {A //B}')
+
+print(f'O resultado de B % A é: {B % A}')
+
+print(f'O resultado de A % B é: {A % B}')
+
+print(f'O resultado de B // A é: {B // A}')
+
+print(f'O resultado de B - A / B é: {round(B - A / B)}') #round arredonda o valor
+
+print(f'O resultado de A / B - A * B é :{round(A / B - A * B)}')
+
+print(10 + A % B)
+
+print(10 - B // A)
+
+print(B // A - 50)
+
+print( A + B % A  + A // B * 2)
+
+print(A * (B + A) - (A - B) / 2)
+
+print('Próximo exercício ')
+# B. Considere as variáveis com os valores X = True e Y = False e calcule as seguintes expressões:
+X = True
+Y = False
+
+print( X and Y)
+
+print( Y or X)
+
+print( X or Y or X)
+
+print(not (X or not Y))
+
+print(not (not X and not Y))
+
+print(not (not X and not Y))
+
+print(not X and Y)
+
+print( not (X and Y))
+
+print( not X and Y or X)
+
+print( not (X and Y) and Y)
+
+print( X and Y or Y and not X)
+
+print(not (X or Y and Y or Y))
+
+print('Último exercício')
+
+# C. Considere as variáveis com os valores A = 5, B = 9, X = not True, Y = False and True, M = “Casa” e N = “Mesa” calcule as seguintes expressões:
+
+A = 5
+B = 9
+X= not True
+Y= False and True
+M= "Casa"
+N= "Mesa"
+
+print(X and Y == X or Y)
+
+print( A * B <= B - A)
+
+print(not (A == B or not B != A))
+
+print(not (M != N and B > A))
+
+print(B + A <= A * B and Y)
+
+print(X and not Y or A // B != A % B)
